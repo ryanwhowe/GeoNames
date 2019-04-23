b0f8e302088dd9cc80c924c3ee336869256430a6
Ryan W. Howe <ryanwhowe@gmail.com>
2019-04-23 09:18:22 -0400

    * reorganized GeoNames namespace

--- GeoNames/GeoNames.py
+++ GeoNames/GeoNames.py
@@ -1,28 +1,3 @@
-
-class GeoNames:
-    def __init__(self):
-        self.DIR = {'downloads': '/Downloads/', 'loaders': '/Loaders/'}
-
-    def getDir(self, basedir, requestdir):
-        return basedir + self.DIR[requestdir]
-
-
-class GeoNamesDump:
-    """Meta and operational code for the GeoNames Dump location
-    There are only specific files that are needed from the GeoNames Dump location and there is no need to scrape all
-    the files and download them since the allCountries.zip contains them all.  Once these files have been downloaded an
-    initial time then only the incremental files are needed to keep the system up to date.
-
-    """
-
-    def __init__(self):
-        self.URL = 'http://download.geonames.org/export/dump/'
-        self.FILES = ['allCountries.zip', 'alternateNames.zip', 'admin2Codes.txt', 'admin1CodesASCII.txt',
-                      'featureCodes_en.txt', 'timeZones.txt', 'countryInfo.txt']
-        self.INCREMENTAL = ['deletes-', 'modifications-', 'alternateNamesDeletes-', 'alternateNamesModifications-']
-
-
-class GeoNamesPostalCodes:
-    def __init__(self):
-        self.URL = 'http://download.geonames.org/export/zip/'
-        self.FILES = ['US.zip']
+def get_dir(basedir, requestdir):
+    directories = {'downloads': '/Downloads/', 'loaders': '/Loaders/'}
+    return basedir + directories[requestdir]
--- download.py
+++ download.py
@@ -1,45 +1,44 @@
-
 import os
 from datetime import datetime, timedelta
 
 import wget
 from tqdm import tqdm
 
-import GeoNames.GeoNames as GN
+from GeoNames import GeoNames, dump
 
 
 def check_create_directory(directory):
     if not os.path.isdir(directory):
+        # todo add some logic to check for write permissions before attempting to create dir
         os.mkdir(directory)
 
 
 def pull_file(sourcefile, desitinationfile):
     if not os.path.isfile(desitinationfile):
+        # wget was the fastest http download implementation I was able to find, tried urllib3 & responses
         # Suppress the default download progress bar which overwrites the overall progress bar from tqdm
         wget.download(sourcefile, desitinationfile, bar=None)
 
 
 def main():
-    geonames = GN.GeoNames()
-    geonames_dump = GN.GeoNamesDump()
     currentdir = os.path.dirname(os.path.realpath(__file__))
-    downloads = geonames.getDir(currentdir, 'downloads')
+    downloads = GeoNames.get_dir(currentdir, 'downloads')
 
     check_create_directory(downloads)
 
-    total = len(geonames_dump.FILES) + len(geonames_dump.INCREMENTAL)
+    total = len(dump.FILES) + len(dump.INCREMENTAL)
 
     with tqdm(total=total) as pbar:
         # first process the static files, if present we skip them and move onto incremental files
-        for file in geonames_dump.FILES:
-            sourcefilename = geonames_dump.URL + file
+        for file in dump.FILES:
+            sourcefilename = dump.URL + file
             destinationfilename = downloads + file
             pull_file(sourcefilename, destinationfilename)
             pbar.update(1)
 
-        for file in geonames_dump.INCREMENTAL:
+        for file in dump.INCREMENTAL:
             # construct the filename from todays date
-            sourcefilename = geonames_dump.URL + file + (datetime.today() - timedelta(1)).strftime('%Y-%m-%d') + '.txt'
+            sourcefilename = dump.URL + file + (datetime.today() - timedelta(1)).strftime('%Y-%m-%d') + '.txt'
             destinationfilename = downloads + file + (datetime.today() - timedelta(1)).strftime('%Y-%m-%d') + '.txt'
             pull_file(sourcefilename, destinationfilename)
             pbar.update(1)
