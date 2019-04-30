bae800bc76360eb79e2442885a0cb217aa81f467
Ryan W. Howe <ryanwhowe@gmail.com>
2019-04-30 10:48:09 -0400

    * created the processing queue readers for each queue. * created a script to execute all readers and detach them to the background

---
+++ process_daily_alternateNamesModifications.py
@@ -0,0 +1,66 @@
+import json
+import os
+
+import mysql.connector
+import pika
+from dotenv import load_dotenv
+
+load_dotenv()
+
+database_config = {
+    'host': os.getenv('DATABASE_HOST'),
+    'password': os.getenv('DATABASE_PASS'),
+    'user': os.getenv('DATABASE_USER'),
+    'database': os.getenv('DATABASE_NAME')
+}
+conn = mysql.connector.connect(**database_config)
+conn.sql_mode = ''
+cursor = conn.cursor()
+
+rabbit_user = os.getenv('RABBIT_USER')
+rabbit_pass = os.getenv('RABBIT_PASS')
+credentials = pika.PlainCredentials(rabbit_user, rabbit_pass)
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(
+        host='192.168.2.101',
+        credentials=credentials
+    )
+)
+channel = connection.channel()
+
+
+def modification_process(ch, method, props, body):
+    data = json.loads(body)
+    if conn.is_connected():
+        query = """
+        INSERT INTO `alternatename`(`alternatenameId`, `geonameid`, `isoLanguage`, `alternateName`, `isPreferredName`, `isShortName`, `isColloquial`, `isHistoric`) VALUES 
+        (%s,%s,%s,%s,%s,%s,%s,%s)
+        ON DUPLICATE KEY UPDATE
+            `geonameid` = VALUES(`geonameid`), 
+            `isoLanguage` = VALUES(`isoLanguage`), 
+            `alternateName` = VALUES(`alternateName`), 
+            `isPreferredName` = VALUES(`isPreferredName`), 
+            `isShortName` = VALUES(`isShortName`), 
+            `isColloquial` = VALUES(`isColloquial`), 
+            `isHistoric` = VALUES(`isHistoric`)
+        """
+        insert_tuple = (data['alternatenameId'], data['geonameid'], data['isoLanguage'],
+                        data['alternateName'], data['isPreferredName'], data['isShortName'],
+                        data['isColloquial'], data['isHistoric']
+                        )
+        cursor.execute(query, insert_tuple)
+        conn.commit()
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+
+def modifications():
+    channel.basic_qos(prefetch_count=1)
+    channel.basic_consume(queue='alternateNamesModifications', on_message_callback=modification_process)
+    channel.start_consuming()
+
+
+def main():
+    modifications()
+
+
+main()
---
+++ process_daily_deletes.py
@@ -0,0 +1,52 @@
+import json
+import os
+
+import mysql.connector
+import pika
+from dotenv import load_dotenv
+
+load_dotenv()
+
+database_config = {
+    'host': os.getenv('DATABASE_HOST'),
+    'password': os.getenv('DATABASE_PASS'),
+    'user': os.getenv('DATABASE_USER'),
+    'database': os.getenv('DATABASE_NAME')
+}
+conn = mysql.connector.connect(**database_config)
+conn.sql_mode = ''
+cursor = conn.cursor()
+
+rabbit_user = os.getenv('RABBIT_USER')
+rabbit_pass = os.getenv('RABBIT_PASS')
+credentials = pika.PlainCredentials(rabbit_user, rabbit_pass)
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(
+        host='192.168.2.101',
+        credentials=credentials
+    )
+)
+channel = connection.channel()
+
+
+def delete_process(ch, method, props, body):
+    data = json.loads(body)
+    if conn.is_connected():
+        query = "DELETE FROM geoname WHERE geonameid = %s"
+        delete_tuple = (data['geonameid'],)
+        cursor.execute(query, delete_tuple)
+        conn.commit()
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+
+def deletes():
+    channel.basic_qos(prefetch_count=1)
+    channel.basic_consume(queue='deletes', on_message_callback=delete_process)
+    channel.start_consuming()
+
+
+def main():
+    deletes()
+
+
+main()
