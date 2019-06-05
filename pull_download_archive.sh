d7a5e3f96a8d5aa9e9b9680b3c9049667fea4395
Ryan W. Howe <ryanwhowe@gmail.com>
2019-06-05 13:50:43 -0400

    Added pull_download_archive.sh to repo

---
+++ pull_download_archive.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+cd /Users/axe/Documents/Projects/GeoNames/Downloads
+
+scp home:/home/axe/GeoNames/Downloads/alternateNamesD* /Users/axe/Documents/Projects/GeoNames/Downloads/
+scp home:/home/axe/GeoNames/Downloads/alternateNamesM* /Users/axe/Documents/Projects/GeoNames/Downloads/
+scp home:/home/axe/GeoNames/Downloads/dele* /Users/axe/Documents/Projects/GeoNames/Downloads/
+scp home:/home/axe/GeoNames/Downloads/mod* /Users/axe/Documents/Projects/GeoNames/Downloads/
